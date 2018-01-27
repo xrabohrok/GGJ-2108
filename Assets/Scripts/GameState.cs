6cc290281348e890d98aa801768d2764a10f8b8a
William Norris <emcskid@gmail.com>
2018-01-26 22:42:56 -0600

    base gamestate commit
    Small commit

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class GameState : MonoBehaviour {
+
+    enum Control {Robot, Hack};
+
+    public static GameState instance = null;
+
+    //TODO hide mouse 
+    //TODO add in reference to robot on grid
+    //TODO add in reference to hacking minigame
+    private int robotHealth;
+    private int playerMoves;
+    private int inControl;
+
+
+    //Awake is always called before
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
