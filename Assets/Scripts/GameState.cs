696176b40facdf9646e8825339a015de6616d256
William Norris <emcskid@gmail.com>
2018-01-27 11:49:17 -0600

    GameState skeleton
    TODO: Add in references to Hack and robot Controller

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class GameState : MonoBehaviour {
@@ -10,13 +11,11 @@ public class GameState : MonoBehaviour {
     public static GameState instance = null;
     public GameObject hackGame;
     public GameObject robotGame;
+    public Text controlSwitchText;
 
-
-    //TODO hide mouse 
-    //TODO add in reference to robot on grid
-    //TODO add in reference to hacking minigame
-    private int robotHealth;
-    private int playerMoves;
+    private int robotHealth = 5;
+    private int playerMoves = 5;
+    
 
 
     //Awake is always called before
@@ -29,8 +28,13 @@ public class GameState : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
+        //TODO add in reference to robot on grid
         //hackGame = GameObject.Find("<HACKING BOARD>");
+        //TODO add in reference to hacking minigame
         //robotGame = GameObject.Find("<ROBOT GAME>");
+
+        controlSwitchText.text = "Controlling Robot";
+
 	}
 	
 	// Update is called once per frame
@@ -39,24 +43,14 @@ public class GameState : MonoBehaviour {
 
 	}
 
-    Control ChangeControl (Control con)
-    {
-        if (con == Control.Hack)
-        {
-            con = Control.Robot;
-        }
-        else if (con == Control.Robot)
-        {
-            con = Control.Hack;
-        }
-
-        return con;
-    }
-
     void ChangeToRobot()
     {
         Cursor.visible = false;
-       // hackGame.SetActive(true);
+
+        controlSwitchText.text = "Controlling Robot";
+        //hackController.SetActive(false);
+        //robotController.SetActive(true);
+        // hackGame.SetActive(true);
 
     }
 
@@ -64,7 +58,13 @@ public class GameState : MonoBehaviour {
     {
 
         Cursor.visible = true;
+
+        controlSwitchText.text = "Hacking";
+        //hackController.SetActive(true);
+        //robotController.SetActive(false);
         //robotGame.SetActive(true);
 
     }
+
+
 }
