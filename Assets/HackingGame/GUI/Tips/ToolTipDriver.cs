dac010a2f7bb961d9dee0546faa00ed207197107
Mr.Black <westin007@gmail.com>
2018-02-10 15:09:15 -0600

    Tooltip generator now made - Mr.Black

--- Assets/HackingGame/GUI/Tips/ToolTipDriver.cs
+++ Assets/HackingGame/GUI/Tips/ToolTipDriver.cs
@@ -1,28 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Assets.common;
 using UnityEngine;
 
-public class ToolTipDriver : MonoBehaviour {
+public class ToolTipDriver : MonoBehaviour
+{
+    public List<Sprite> AllTips;
+
     private Clickable dismissButton;
     private Animator anim;
 
+    private static RandomSpinner<Sprite> spinner;
+    private SpriteRenderer tipSprite;
+
     // Use this for initialization
-	void Start ()
-	{
-	    var allClickables = GetComponentsInChildren<Clickable>();
-	    foreach (var clickable in allClickables)
-	    {
-            Debug.Log(clickable.gameObject.name);
-	        if (clickable.gameObject.name == "dismissButton")
-	        {
-	            dismissButton = clickable;
-	        }
-	    }
+    void Start()
+    {
+        var allClickables = GetComponentsInChildren<Clickable>();
+        foreach (var clickable in allClickables)
+        {
+            if (clickable.gameObject.name == "dismissButton")
+            {
+                dismissButton = clickable;
+            }
+        }
         dismissButton.setClickReleaseCallback(dismiss);
 
-	    anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
 
-	}
+        //choose random sprite
+        if (spinner == null)
+        {
+            spinner = new RandomSpinner<Sprite>();
+            AllTips.ForEach(t => spinner.addNewPossibility(5, t));
+        }
+
+        var things = this.GetComponentsInChildren<SpriteRenderer>();
+        foreach (var renderer in things)
+        {
+            if (renderer.gameObject.name == "tip")
+            {
+                tipSprite = renderer;
+            }
+        }
+
+        tipSprite.sprite = spinner.getRandom();
+
+    }
 
     public void dismiss()
     {
