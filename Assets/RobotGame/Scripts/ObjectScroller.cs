82bf5d87399c730cb51319ba79c9f31e48e779dc
drfro <rimcnei@gmail.com>
2018-02-10 16:26:14 -0600

    Renamed background script to ObjectScroller, adding an event to test scrolling

---
+++ Assets/RobotGame/Scripts/ObjectScroller.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ObjectScroller : MonoBehaviour
+{
+    // Scroll speed
+    public float speed = 0.25f;
+
+    //The Y point to reset the scrolling object
+    public float resetAtY = 1;
+
+    void Update()
+    {
+        // Value of Y change from 0 to resetAtY by time. return to 0 if it becomes greater then resetAtY.
+        float y = Mathf.Repeat(Time.time * speed, resetAtY);
+
+        // Create offset that shift value of Y
+        Vector3 offset = new Vector3(0, y, 0);
+
+        GetComponent<Renderer>().transform.position = offset;
+    }
+
+    public void SetSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+    }
+}
