fdc0bce102dedbdcc22b71d0030f3e862f180509
drfro <rimcnei@gmail.com>
2018-02-10 17:03:21 -0600

    ObjectScroller now works for neg Y values

--- Assets/RobotGame/Scripts/ObjectScroller.cs
+++ Assets/RobotGame/Scripts/ObjectScroller.cs
@@ -3,20 +3,41 @@
 public class ObjectScroller : MonoBehaviour
 {
     // Scroll speed
-    public float speed = 0.25f;
+    public float speed = 5;
 
     //The Y point to reset the scrolling object
     public float resetAtY = 1;
 
+    public float startAtY = 0;
+
     void Update()
     {
+        Renderer objectRenderer = GetComponent<Renderer>();
+
         // Value of Y change from 0 to resetAtY by time. return to 0 if it becomes greater then resetAtY.
-        float y = Mathf.Repeat(Time.time * speed, resetAtY);
+        //float offsetY = Mathf.Repeat(Time.time * speed, resetAtY);
+
+        //Debug.Log("OffsetY:" + offsetY);
+
+        float currentY = objectRenderer.transform.position.y;
+        //Debug.Log("CurrentY:" + currentY);
+
+        //Turns the speed from human Friendly to the real speed
+        float realSpeed = speed / 1000;
+
+        float newY = currentY + realSpeed;
+
+        if(newY >= resetAtY)
+        {
+            newY = startAtY;
+        }
+
+       // Debug.Log("NewY:" + newY);
 
         // Create offset that shift value of Y
-        Vector3 offset = new Vector3(0, y, 0);
+        Vector3 offset = new Vector3(0, newY, 0);
 
-        GetComponent<Renderer>().transform.position = offset;
+        objectRenderer.transform.position = offset;
     }
 
     public void SetSpeed(float newSpeed)
