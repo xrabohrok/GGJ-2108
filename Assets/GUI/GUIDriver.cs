6bc6a1f20b84143d1beaeea69b215afdbe6ce8f5
Mr.Black <westin007@gmail.com>
2018-02-10 21:42:50 -0600

    made starbar gui - Mr.Black

---
+++ Assets/GUI/GUIDriver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GUIDriver : MonoBehaviour
+{
+
+    public int quitLevelIndex;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void quit()
+    {
+        SceneManager.LoadScene(quitLevelIndex);
+
+    }
+}
