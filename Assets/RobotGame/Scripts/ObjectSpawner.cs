9a1afdb95090a73f321a398ef556f35a01e1c08b
Matthew Wilmsmeyer <mwilmsmeyer@revolutionehr.com>
2018-02-10 20:09:23 -0600

    Created file -mw

---
+++ Assets/RobotGame/Scripts/ObjectSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour {
+
+    public static ObjectSpawner instance;
+    public List<ObjectScroller> randomObjects;
+
+	void Awake() {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+	}
+	
+	void Update () {
+		if (this.randomObjects.Count < 2)
+        {
+            int randTime = Random.Range(2, 4);
+        }
+	}
+}
