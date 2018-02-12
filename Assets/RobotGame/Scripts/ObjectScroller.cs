036562c3feef7df86be2a71a24c87b4de7238372
drfro <rimcnei@gmail.com>
2018-02-11 18:38:52 -0600

    Scrolling background added to combined game

--- Assets/RobotGame/Scripts/ObjectScroller.cs
+++ Assets/RobotGame/Scripts/ObjectScroller.cs
@@ -1,47 +1,57 @@
 ﻿using UnityEngine;
 
-public class ObjectScroller : MonoBehaviour
+namespace RobotGame
 {
-    // Scroll speed
-    public float speed = 5;
-
-    //The Y point to reset the scrolling object
-    public float resetAtY = 1;
-  
-    public float startAtY = 0;
-
-    void Update()
+    public class ObjectScroller : MonoBehaviour
     {
-        Renderer objectRenderer = GetComponent<Renderer>();
+        //The Y point to reset the scrolling object
+        public float resetAtY = 1;
 
-        // Value of Y change from 0 to resetAtY by time. return to 0 if it becomes greater then resetAtY.
-        //float offsetY = Mathf.Repeat(Time.time * speed, resetAtY);
+        public float startAtY = 0;
 
-        //Debug.Log("OffsetY:" + offsetY);
+        private Clanky clanky;
 
-        float currentY = objectRenderer.transform.position.y;
-        //Debug.Log("CurrentY:" + currentY);
-
-        //Turns the speed from human Friendly to the real speed
-        float realSpeed = speed / 1000;
-
-        float newY = currentY + realSpeed;
-
-        if(newY >= resetAtY)
+        private void Start()
         {
-            newY = startAtY;
+            clanky = GameObject.Find("Clanky").GetComponent<Clanky>();
+           
         }
 
-       // Debug.Log("NewY:" + newY);
+        void Update()
+        {
+  
 
-        // Create offset that shift value of Y
-        Vector3 offset = new Vector3(0, newY, 0);
+            Renderer objectRenderer = GetComponent<Renderer>();
 
-        objectRenderer.transform.position = offset;
-    }
+            // Value of Y change from 0 to resetAtY by time. return to 0 if it becomes greater then resetAtY.
+            //float offsetY = Mathf.Repeat(Time.time * speed, resetAtY);
 
-    public void SetSpeed(float newSpeed)
-    {
-        speed = newSpeed;
+            //Debug.Log("OffsetY:" + offsetY);
+
+            float currentY = objectRenderer.transform.localPosition.y;
+            //Debug.Log("CurrentY:" + currentY);
+
+            //Turns the speed from human Friendly to the real speed
+            float realSpeed = GetSpeed() / 1000;
+
+            float newY = currentY + realSpeed;
+
+            if (newY >= resetAtY)
+            {
+                newY = startAtY;
+            }
+
+            // Debug.Log("NewY:" + newY);
+
+            // Create offset that shift value of Y
+            Vector3 offset = new Vector3(0, newY, 0);
+
+            objectRenderer.transform.localPosition = offset;
+        }
+
+        public float GetSpeed()
+        {
+            return clanky.currentSpeed;
+        }
     }
 }
