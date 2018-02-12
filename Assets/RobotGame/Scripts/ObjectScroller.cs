87b31a53c5a9f34216d33d785e558e1f84449609
drfro <rimcnei@gmail.com>
2018-02-11 18:38:52 -0600

    Scrolling background added to combined game

--- Assets/RobotGame/Scripts/ObjectScroller.cs
+++ Assets/RobotGame/Scripts/ObjectScroller.cs
@@ -9,17 +9,25 @@ namespace RobotGame
 
         public float startAtY = 0;
 
+        private Clanky clanky;
 
-    void Update()
-    {
-        Renderer objectRenderer = GetComponent<Renderer>();
+        private void Start()
+        {
+            clanky = GameObject.Find("Clanky").GetComponent<Clanky>();
+           
+        }
+
+
+        void Update()
+        {
+            Renderer objectRenderer = GetComponent<Renderer>();
 
             // Value of Y change from 0 to resetAtY by time. return to 0 if it becomes greater then resetAtY.
             //float offsetY = Mathf.Repeat(Time.time * speed, resetAtY);
 
             //Debug.Log("OffsetY:" + offsetY);
 
-            float currentY = objectRenderer.transform.position.y;
+            float currentY = objectRenderer.transform.localPosition.y;
             //Debug.Log("CurrentY:" + currentY);
 
             //Turns the speed from human Friendly to the real speed
@@ -37,12 +45,12 @@ namespace RobotGame
             // Create offset that shift value of Y
             Vector3 offset = new Vector3(0, newY, 0);
 
-            objectRenderer.transform.position = offset;
+            objectRenderer.transform.localPosition = offset;
         }
 
         public float GetSpeed()
         {
-            return GameObject.Find("Clanky").GetComponent<Clanky>().currentSpeed;
+            return clanky.currentSpeed;
         }
     }
 }
